f1bba5d5dabeb0e3932e2615277b097afbe2964f
Dani Setiawan <mayadani28@gmail.com>
2021-11-11 13:26:22 +0700

    Showing states

--- src/main/java/com/yeahbutstill/manageserver/resource/ServerResource.java
+++ src/main/java/com/yeahbutstill/manageserver/resource/ServerResource.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 
 import static com.yeahbutstill.manageserver.enumeration.Status.SERVER_UP;
 import static java.time.LocalDateTime.now;
@@ -28,7 +29,8 @@ public class ServerResource {
     private final ServerServiceImpl serverService;
 
     @GetMapping("/list")
-    public ResponseEntity<Response> getServers() {
+    public ResponseEntity<Response> getServers() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(3);
         return ResponseEntity.ok(
                 Response.builder()
                         .localDateTime(now())
